a511203f9a9137bc5df62b7401e6a807a9f8ef70
Zainah-Alloush <s130017905@gmail.com>
2017-02-03 01:34:09 +0300

    zaian

---
+++ app/src/main/java/seu/mysqldemo/config.java
@@ -0,0 +1,15 @@
+package seu.mysqldemo;
+
+/**
+ * Created by aspire on 02/02/17.
+ */
+
+public class config {
+
+        // File upload url (replace the ip with your server address)
+        public static final String FILE_UPLOAD_URL = "https://amaken-app.000webhostapp.com/AndroidFileUpload/uploads/fileUpload.php";
+
+        // Directory name to store captured images and videos
+        public static final String IMAGE_DIRECTORY_NAME = "AndroidFileUpload";
+    }
+
